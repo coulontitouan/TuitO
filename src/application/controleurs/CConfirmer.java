58b88c4f31ec8662093877f7373c56482f3b25c2
Livreur <coulontitouann@gmail.com>
2024-01-21 05:43:05 +0100

    bouml

--- src/application/controleurs/CConfirmer.java
+++ src/application/controleurs/CConfirmer.java
@@ -2,11 +2,6 @@
 package application.controleurs;
 
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.FileWriter;
